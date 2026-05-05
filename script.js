1cbedb126efe5308d6f1cacb681fe0bab02ccb6c
Mery-njz <m.agostinho@aluno.ifsp.edu.br>
2026-05-05 08:52:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 function alterarFundo(){
   document.body.style.backgroundColor = "black";
 }
+
+const form = document.querySelector("form");
+const saida = document.createElement("div");
+document.body.appendChild(saida);
+
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+
+  // cap
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const mensagem = document.getElementById("mensagem").value;
+  const categoria = document.getElementById("categoria").value;
+
+  // mon
+  const dados = {
+    nome: nome,
+    email: email,
+    mensagem: mensagem,
+    categoria: categoria
+  };
+
+  // exi
+  saida.innerHTML = `
+    <p><strong>Nome:</strong> ${dados.nome}</p>
+    <p><strong>Email:</strong> ${dados.email}</p>
+    <p><strong>Mensagem:</strong> ${dados.mensagem}</p>
+    <p><strong>Categoria:</strong> ${dados.categoria}</p>
+  `;
+});
+
